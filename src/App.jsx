b659bb76add728f8dd0f567c0d0fd84c23a70724
Abhilash037415 <abhilashmunnangi12@gmail.com>
2025-08-31 13:15:49 +0530

    added landing page

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import Home from "./pages/Home";
 import SignInForm from "./pages/Signin";
 import Signup from "./pages/signup";
+import Features from "./pages/Features";
 
 export default function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Signup />} />
+        <Route path="/" element={<Home />} />
         <Route path="/signin" element={<SignInForm />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/features" element={<Features />} /> 
       </Routes>
     </Router>
   );
